7cecc7c0baac6126ce1c669579d575e1ed572330
Sakshi Sharma <sakshisharma.cse19@chitkarauniversity.edu.in>
2022-12-05 12:16:41 +0530

    Resolved: Cors Issue making it public api

--- routes/cors.js
+++ routes/cors.js
@@ -1,21 +1,9 @@
-const allowlist = ['http://localhost:3000/'];
 
 const corsOptionsDelegate = (req, callback) => {
     let corsOptions;
+   
+    corsOptions = { origin: true }
 
-    let isDomainAllowed = allowlist.indexOf(req.header('Origin')) !== -1;
-    let isExtensionAllowed = req.path.endsWith('.jpg');
-
-    if (isDomainAllowed && isExtensionAllowed) {
-        console.log("this is working ");
-        // Enable CORS for this request
-        corsOptions = { origin: true }
-    } else {
-        // Disable CORS for this request
-        console.log("this is not working ");
-
-        corsOptions = { origin: true }
-    }
     callback(null, corsOptions)
 }
 module.exports = corsOptionsDelegate;
